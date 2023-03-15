333bf62d61cd8a2c3c21e1678921e4b0ad978050
Ivan Bereziuk <Navi.Bereziuk@gmail.com>
2023-03-15 22:34:33 +0100

    begin on job runner

--- core/job_runner/main.cpp
+++ core/job_runner/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 #include "vt100.h"
@@ -14,12 +15,80 @@ struct SortJobDescription
     Sorter sorter;
 };
 
+struct RunSequenceScore
+{
+    struct RunScore{
+        size_t N;
+        size_t timeNs;
+
+        RunScore(size_t n, size_t t)
+            : N(n)
+            , timeNs(t)
+        {}
+    };
+
+    std::string name;
+    std::vector<RunScore> runs;
+};
+
+
 int main(int /*argc*/, char* /*argv*/[])
 {
+    std::cout << "Let the show begin..\n";
+
     std::vector<SortJobDescription> sortSeries{
         {"quick_sort", alex_sort},
         {"Alex_sort", alex_sort}
     };
+
+    constexpr size_t N_MAX = 100000;
+    
+    std::vector<RunSequenceScore> resutls;
+    
+    // create
+    //for (const SortJobDescription& desc : sortSeries)
+    //{
+    //    resutls.push_back({ desc.name });
+    //    //std::vector<RunSequenceScore::RunScore>& sequence = resutls.back().runs;
+    //}
+
+    for (const SortJobDescription& desc : sortSeries)
+    {
+        resutls.push_back({ desc.name });
+        RunSequenceScore& sequence = resutls.back();
+
+        const SortJobDescription::Sorter theSort = desc.sorter;
+        for (size_t n = 10; n <= N_MAX; n *= 10)
+        {
+            // Prepare
+            std::vector<int32_t> arrayToSort = generateRandomArray(n);
+            
+            // Act
+            // start measure
+            theSort(arrayToSort.data(), arrayToSort.size());
+
+            size_t timeNs = 100;
+            // end measure
+
+            sequence.runs.emplace_back(n, timeNs);
+        }
+    }
+
+
+    // present the results
+
+    for (const RunSequenceScore& sequence : resutls)
+    {
+        std::cout << std::setw(10) << sequence.name;
+
+        for (const auto& run : sequence.runs)
+        {
+            std::cout << std::setw(10) << run.timeNs;
+            std::cout << "(" << run.N << ")";
+        }
+
+        std::cout << "\n";
+    }
 
     return 0;
 }
